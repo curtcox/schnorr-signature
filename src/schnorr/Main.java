9f29dbc4f71fc64e80094fc35f10b8082d36b24f
Curt Cox <curtcox@gmail.com>
2018-09-19 22:27:20 -0500

    Refactor.

--- src/schnorr/Main.java
+++ src/schnorr/Main.java
@@ -4,42 +4,54 @@ import java.util.*;
 
  class Main {
 
+     int type;
+     KeyPair keyPair;
+     Signature signature;
+     byte[] bytes;
+     final Scanner s = new Scanner(System.in);
+
     public static void main(String[] args) {
-
-        Scanner s = new Scanner(System.in);
-        println("1 <bits in q> - generate keys");
-        println("2 <message> - sign");
-        println("3 - check signature");
-        println("4 exit");
-
-        int type;
-        KeyPair keyPair = null;
-        Signature signature = null;
-        byte[] bytes = null;
-        while (true) {
-            type = s.nextInt();
-            if (type == 1) {
-                int blq = s.nextInt();
-                keyPair = KeyPair.generate(blq);
-                println("keys = " + keyPair);
-            }
-            if (type == 2) {
-                bytes = s.next().getBytes();
-                signature = keyPair.sign(bytes);
-                println("signature = " + signature);
-            }
-            if (type == 3) {
-                if (signature.check(bytes,keyPair.publicKey)) {
-                    println("Signature is valid");
-                } else {
-                    println("Signature is not valid");
-                }
-            }
-            if (type == 4){
-                break;
-            }
-        }
+         Main main = new Main();
+         main.run();
     }
+
+    void run() {
+        printInstructions();
+        loopForInput();
+    }
+
+     void loopForInput() {
+         while (true) {
+             type = s.nextInt();
+             if (type == 1) {
+                 int blq = s.nextInt();
+                 keyPair = KeyPair.generate(blq);
+                 println("keys = " + keyPair);
+             }
+             if (type == 2) {
+                 bytes = s.next().getBytes();
+                 signature = keyPair.sign(bytes);
+                 println("signature = " + signature);
+             }
+             if (type == 3) {
+                 if (signature.check(bytes,keyPair.publicKey)) {
+                     println("Signature is valid");
+                 } else {
+                     println("Signature is not valid");
+                 }
+             }
+             if (type == 4){
+                 break;
+             }
+         }
+     }
+
+     void printInstructions() {
+         println("1 <bits in q> - generate keys");
+         println("2 <message> - sign");
+         println("3 - check signature");
+         println("4 exit");
+     }
 
      static void println(String message) {
          System.out.println(message);
