67ee1bd4a19e81989ffc2b7ecd4fcb672ab0f028
Curt Cox <curtcox@gmail.com>
2018-09-20 20:13:25 -0500

    Extract variables.

--- src/schnorr/KeyPairGenerator.java
+++ src/schnorr/KeyPairGenerator.java
@@ -8,6 +8,8 @@ final class KeyPairGenerator {
     private final int bitLength;
     private final SecureRandom sr = new SecureRandom();
     private static final int certainty = 100;
+    private static final BigInteger one = new BigInteger("1");
+    private static final BigInteger two = new BigInteger("2");
 
     public KeyPairGenerator(int bitLength) {
         this.bitLength = bitLength;
@@ -17,40 +19,37 @@ final class KeyPairGenerator {
         BigInteger q = randomPrime();
         BigInteger p = compute_p(q);
         BigInteger g = compute_g(p,q);
-        BigInteger w = new BigInteger(bitLength, sr);
+        BigInteger w = random();
         BigInteger y = g.modPow(w, p);
 
         return new KeyPair(new PublicKey(q, p, g, y), new PrivateKey(w));
     }
 
-    BigInteger randomPrime() {
-        return new BigInteger(bitLength, certainty, sr);
-    }
+    BigInteger random() { return new BigInteger(bitLength, sr); }
+    BigInteger randomPrime() { return new BigInteger(bitLength, certainty, sr); }
 
     private BigInteger compute_p(BigInteger q) {
-        BigInteger one = new BigInteger("1");
-        BigInteger two = new BigInteger("2");
-        BigInteger qp = BigInteger.ONE;
+        BigInteger qp = one;
         BigInteger p;
         do {
             p = q.multiply(qp).multiply(two).add(one);
-            if (p.isProbablePrime(certainty)) break;
-            qp = qp.add(BigInteger.ONE);
+            if (p.isProbablePrime(certainty)) {
+                return p;
+            }
+            qp = qp.add(one);
         } while (true);
-        return p;
     }
 
     private BigInteger compute_g(BigInteger p, BigInteger q) {
         BigInteger g;
         while (true) {
-            BigInteger two = new BigInteger("2");
             BigInteger  a = (two.add( randomPrime() )).mod(p);
-            BigInteger ga = (p.subtract(BigInteger.ONE)).divide(q);
+            BigInteger ga = (p.subtract(one)).divide(q);
             g = a.modPow(ga, p);
-            if (g.compareTo(BigInteger.ONE) != 0)
-                break;
+            if (g.compareTo(one) != 0) {
+                return g;
+            }
         }
-        return g;
     }
 
 }
