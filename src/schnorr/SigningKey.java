3fa7e8fe238ea7e318dad896901a59a12a948228
Curt Cox <curtcox@gmail.com>
2018-09-25 22:32:04 -0500

    Extract method.

--- src/schnorr/SigningKey.java
+++ src/schnorr/SigningKey.java
@@ -15,19 +15,21 @@ final class SigningKey {
 
     Signature sign(byte[] bytes) {
         BigInteger q = seed.q;
-        BigInteger p = seed.p;
-        BigInteger g = seed.g;
         BigInteger w = privateKey.w;
+        BigInteger r = new BigInteger(q.bitLength(), new SecureRandom());
 
-        SecureRandom sr = new SecureRandom();
-
-        BigInteger r = new BigInteger(q.bitLength(), sr);
-        BigInteger x = g.modPow(r, p);
-
-        BigInteger s1 = Util.bigMd5(bytes,x);
+        BigInteger s1 = s1(bytes,r);
         BigInteger s2 = r.subtract(w.multiply(s1)).mod(q);
 
         return new Signature(s1, s2);
+    }
+
+    BigInteger s1(byte[] bytes,BigInteger r) {
+        BigInteger p = seed.p;
+        BigInteger g = seed.g;
+        BigInteger x = g.modPow(r, p);
+
+        return Util.bigMd5(bytes,x);
     }
 
     public String toString() {
